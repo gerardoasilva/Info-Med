ea66a7f140e70d08dc8a3a502eb432adff3ec672
Gerardo Silva <gsilva.razo@gmail.com>
2020-04-20 21:30:37 -0500

    Initial Commit

---
+++ Info-MedTests/Info_MedTests.swift
@@ -0,0 +1,34 @@
+//
+//  Info_MedTests.swift
+//  Info-MedTests
+//
+//  Created by Gerardo Silva Razo on 4/20/20.
+//  Copyright © 2020 Tecnológico de Monterrey. All rights reserved.
+//
+
+import XCTest
+@testable import Info_Med
+
+class Info_MedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
