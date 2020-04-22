6f88626cd90df0af9fb7bbe7a7872065ba01c1f6
Gerardo Silva <gsilva.razo@gmail.com>
2020-04-21 19:11:44 -0500

    Add Login and Register

---
+++ Pods/Target Support Files/Pods-Info-Med-Info-MedUITests/Pods-Info-Med-Info-MedUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Info_Med_Info_MedUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Info_Med_Info_MedUITests
+@end
---
+++ Pods/Target Support Files/Pods-Info-MedTests/Pods-Info-MedTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Info_MedTests : NSObject
+@end
+@implementation PodsDummy_Pods_Info_MedTests
+@end
