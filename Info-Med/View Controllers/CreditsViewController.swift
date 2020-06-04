8c6f1f43d0f9dcfbc4a14a4282345eb0d60f3887
Luis Alberto Caballero Noguez <luis_cn98@hotmail.com>
2020-06-03 20:26:57 -0700

    Added navigation to credit VC

---
+++ Info-Med/View Controllers/CreditsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreditsViewController.swift
+//  Info-Med
+//
+//  Created by user168608 on 6/3/20.
+//  Copyright © 2020 Tecnológico de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
